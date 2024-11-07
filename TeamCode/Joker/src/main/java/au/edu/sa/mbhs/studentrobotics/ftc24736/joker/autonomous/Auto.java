e9c5bc1a052a89df02ab69d370af8ef9d9e03922
username-button <cdprofgo@gmail.com>
2024-11-07 16:49:54 +1030

    added one of the MeepMeep paths to autonomous, it no working though

--- TeamCode/Joker/src/main/java/au/edu/sa/mbhs/studentrobotics/ftc24736/joker/autonomous/Auto.java
+++ TeamCode/Joker/src/main/java/au/edu/sa/mbhs/studentrobotics/ftc24736/joker/autonomous/Auto.java
@@ -1,13 +1,16 @@
 package au.edu.sa.mbhs.studentrobotics.ftc24736.joker.autonomous;
 
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Degrees;
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Inches;
+
 import androidx.annotation.Nullable;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.AutonomousBunyipsOpMode;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.Reference;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.localization.MecanumLocalizer;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.roadrunner.parameters.DriveModel;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.ftc24736.joker.Joker;
 
@@ -18,16 +21,11 @@ public class Auto extends AutonomousBunyipsOpMode {
     @Override
     protected void onInitialise() {
         robot.init();
-        DriveModel dm = new DriveModel.Builder()
-
-                .build();
-        robot.drive
-                .withLocalizer(new MecanumLocalizer(dm, robot.frontLeft, robot.backLeft, robot.backRight, robot.frontRight, robot.imu.get()));
     }
 
     @Override
     protected void onReady(@Nullable Reference<?> selectedOpMode, Controls selectedButton) {
-        add(robot.lift.tasks.goTo(robot.handoverPoint));
+        //add(robot.lift.tasks.goTo(robot.handoverPoint));
 
         /*
         //MeepMeep path (drops preloaded sample in the basket, then grabs and places 2 more):
@@ -43,6 +41,34 @@ public class Auto extends AutonomousBunyipsOpMode {
         .waitSeconds(2)
         .strafeToLinearHeading(new Vector2d(24*2, 24*2), Inches, 45, Degrees)
         .addTask();
+
+        //MeepMeep path (drops preloaded sample in the basket and pushes the floor samples into the basket zone)
+
+        drive.makeTrajectory(new Pose2d(24*1.5, 24*3-9, Math.toRadians(270)))
+                .strafeToLinearHeading(new Vector2d(24*2, 24*2), Inches, 45, Degrees)
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(40, 24), Inches, 270, Degrees)
+                .strafeTo(new Vector2d(24*2, 0), Inches)
+                .strafeTo(new Vector2d(24*2+6, 0), Inches)
+                .strafeTo(new Vector2d(24*2+6, 24*2.2), Inches)
+                .strafeTo(new Vector2d(24*2+6, 0), Inches)
+                .strafeTo(new Vector2d(61, 0), Inches)
+                .strafeTo(new Vector2d(61, 24*2.2), Inches)
+                .addTask();
         */
+
+        robot.drive.setPose(new Pose2d(24*1.5, 24*3-9, Math.toRadians(270)));
+
+        robot.drive.makeTrajectory(new Pose2d(24*1.5, 24*3-9, Math.toRadians(270)))
+                .strafeToLinearHeading(new Vector2d(24*2, 24*2), Inches, 45, Degrees)
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d(40, 24), Inches, 270, Degrees)
+                .strafeTo(new Vector2d(24*2, 0), Inches)
+                .strafeTo(new Vector2d(24*2+6, 0), Inches)
+                .strafeTo(new Vector2d(24*2+6, 24*2.2), Inches)
+                .strafeTo(new Vector2d(24*2+6, 0), Inches)
+                .strafeTo(new Vector2d(61, 0), Inches)
+                .strafeTo(new Vector2d(61, 24*2.2), Inches)
+                .addTask();
     }
 }
